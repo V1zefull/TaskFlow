81fd6707002a27b2960faea4fc5f684ae9250448
Borovlev Egor <eab@promtexklg.ru>
2026-08-07 10:46:15 +0300

    feat: Добавление карточки с задачей

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import Header from "@/components/header/Header";
 import TaskCardAddModal from "@/components/taskCard/modal/TaskCardAddModal";
+import { useTaskStore } from "./store/useTaskStore";
+import TaskBoard from "./components/taskBoard/TaskBoard";
 
 function App() {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
+  const addTask = useTaskStore((state) => state.addTask);
   return (
     <>
       <Header onAddTaskClick={() => setIsAddModalOpen(true)} />
+      <main>
+        <TaskBoard />
+      </main>
       <TaskCardAddModal
         isOpen={isAddModalOpen}
         onClose={() => setIsAddModalOpen(false)}
-        onSubmit={(data) => {
-          console.log(data);
-          setIsAddModalOpen(false);
-        }}
+        onSubmit={addTask}
       />
     </>
   );
